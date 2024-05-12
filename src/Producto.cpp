a6f9aaef3699101008dd3e45418b6d15d3c85ee6
Fabrich21 <146675966+Fabrich21@users.noreply.github.com>
2024-05-12 15:51:00 -0400

    revisar

---
+++ src/Producto.cpp
@@ -0,0 +1,39 @@
+#include "Producto.h"
+
+// Constructor
+Producto::Producto(std::string nombre, 
+                  std::string categoria, 
+                  std::string subCategoria, 
+                  float precio, 
+                  int id): 
+{
+    this -> nombre = nombre;
+    this -> categoria = categoria;
+    this -> subCategoria = subCategoria;
+    this -> precio = precio;
+    this -> id = id;
+  
+};
+//destructor
+Producto::~Producto(){};
+
+// Métodos Getters
+std::string Producto::getNombre() {
+    return nombre;
+}
+
+std::string Producto::getCategoria(){
+    return categoria;
+}
+
+std::string Producto::getSubCategoria(){
+    return subCategoria;
+}
+
+float Producto::getPrecio(){
+    return precio;
+}
+
+int Producto::getID(){
+    return id;
+}
